2bd7e23b3696c440e30e091d2fe5e63176ec3602
Michał Kłos <mhks@gft.com>
2024-04-09 17:45:56 +0200

    Adds tests for API controller

--- TopStories.API.Tests/ControllerTest.cs
+++ TopStories.API.Tests/ControllerTest.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TopStories.API.Controllers;
-using TopStories.API;
-using TopStories.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
-using NUnit.Framework;
 using TopStories.Common.Models;
 using TopStories.Services.TopStoriesService;
 
