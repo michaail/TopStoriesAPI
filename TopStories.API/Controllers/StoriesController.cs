480c721109cdca75095c297bfab8ce414b82d543
Michał Kłos <mhks@gft.com>
2024-04-09 12:16:00 +0200

    Verbose logging for controller

--- TopStories.API/Controllers/StoriesController.cs
+++ TopStories.API/Controllers/StoriesController.cs
@@ -20,18 +20,22 @@ public class StoriesController : ControllerBase
     [HttpGet(Name = "TopStories")]
     public async Task<IEnumerable<Story>> Get(int numberOfStories)
     {
-        _logger.LogInformation($"Received request for: {numberOfStories} most recent stories");
+        _logger.LogInformation($"[StoriesController] - Received request for: {numberOfStories} most recent stories");
+        DateTime start = DateTime.Now;
         var identifiers = await _topStoriesService.GetTopIdentifiers();
-        if(identifiers != null)
+        if (identifiers != null)
         {
             var tasks = new List<Task<Story>>();
             var ids = identifiers.ToList();
 
-            for(int i = 0; i < numberOfStories; i++)
+            for (int i = 0; i < numberOfStories; i++)
             {
                 tasks.Add(_topStoriesService.GetStory(ids[i]));
             }
 
+            var result = await Task.WhenAll(tasks);
+            var duration = TimeSpan.FromTicks(DateTime.Now.Ticks - start.Ticks);
+            _logger.LogInformation($"[StoriesController] - Returned {result.Count()} in {(duration.Milliseconds == 0 ? $"{duration.TotalMicroseconds}us" : $"{ duration.Milliseconds}ms")}");
             return await Task.WhenAll<Story>(tasks);
 
         }
