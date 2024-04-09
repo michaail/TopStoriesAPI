9a020a4143377955ca6f0e5424e95322e30105c4
Michał Kłos <mhks@gft.com>
2024-04-09 15:28:27 +0200

    Adds test project for TopStories.Services

---
+++ TopStories.Services.Tests/Utils/TestDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+
+namespace TopStories.Services.Tests.Utils
+{
+    internal static class TestDataProvider
+    {
+        private static readonly string pathPrefix = "Resources";
+
+        internal static T GetTestData<T>(string testDataFile)
+        {
+            using (StreamReader r = new($"{pathPrefix}/{testDataFile}.json"))
+            {
+                string data = r.ReadToEnd();
+                return JsonSerializer.Deserialize<T>(data)!;
+            }
+        }
+    }
+}
