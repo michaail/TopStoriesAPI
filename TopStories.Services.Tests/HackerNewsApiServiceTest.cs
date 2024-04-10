926b3ca299a6a7654d37585339a0776a3e9689a4
Michał Kłos <mhks@gft.com>
2024-04-10 13:41:16 +0200

    Fix for failing tests

--- TopStories.Services.Tests/HackerNewsApiServiceTest.cs
+++ TopStories.Services.Tests/HackerNewsApiServiceTest.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using TopStories.Common.Helpers;
 using TopStories.Common.Models;
 using TopStories.Services.HackerNewsService;
+using TopStories.Services.Tests.Utils;
 
 namespace TopStories.Tests
 {
@@ -14,35 +15,24 @@ namespace TopStories.Tests
     {
         private Mock<ILogger<HackerNewsAPIService>> _loggerMock;
         private Mock<HttpMessageHandler> _httpMessageHandlerMock;
-        private Mock<StoryConverter> _storyConverterMock;
 
         [SetUp]
         public void SetUp()
         {
-           
             _loggerMock = new Mock<ILogger<HackerNewsAPIService>>();
             _httpMessageHandlerMock = new Mock<HttpMessageHandler>();
-            _storyConverterMock = new Mock<StoryConverter>();
-        
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            // Dispose HttpClient
         }
 
         [Test]
-        [Ignore("httpClient")]
         public async Task GetStory_ValidId_ReturnsStory()
         {
-                        var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
-            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, _storyConverterMock.Object);
+            var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, new StoryConverter());
 
             // Arrange
-            var storyId = 123;
-            var expectedStory = new Story { id = storyId, title = "Test Story" };
-            var json = JsonSerializer.Serialize(expectedStory);
+            var storyId = 39973467;
+            var title = "Llm.c – LLM training in simple, pure C/CUDA";
+            var json = TestDataProvider.GetTestDataString("Story");
             var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(json) };
             _httpMessageHandlerMock.Protected()
                 .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
@@ -54,17 +44,16 @@ namespace TopStories.Tests
             Assert.Multiple(() =>
             {
                 // Assert
-                Assert.That(result.id, Is.EqualTo(expectedStory.id));
-                Assert.That(result.title, Is.EqualTo(expectedStory.title));
+                Assert.That(result.id, Is.EqualTo(storyId));
+                Assert.That(result.title, Is.EqualTo(title));
             });
         }
 
         [Test]
-        [Ignore("httpClient")]
-        public async Task GetStory_InvalidId_ThrowsException()
+        public void GetStory_InvalidId_ThrowsException()
         {
-                        var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
-            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, _storyConverterMock.Object);
+            var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, new StoryConverter());
 
             // Arrange
             var storyId = -1;
@@ -73,15 +62,14 @@ namespace TopStories.Tests
                 .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.NotFound));
 
             // Act & Assert
-            Assert.ThrowsAsync<Exception>(async () => await _hackerNewsApiService.GetStory(storyId));
+            Assert.ThrowsAsync<HttpRequestException>(async () => await _hackerNewsApiService.GetStory(storyId));
         }
 
         [Test]
         public async Task GetTopStoriesIds_ReturnsIds()
         {
-                                    var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
-
-                        var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, _storyConverterMock.Object);
+            var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, new StoryConverter());
 
             // Arrange
             var expectedIds = new List<int> { 1, 2, 3 };
@@ -99,11 +87,10 @@ namespace TopStories.Tests
         }
 
         [Test]
-        [Ignore("httpClient")]
-        public async Task GetTopStoriesIds_FailedRequest_ThrowsException()
+        public void GetTopStoriesIds_FailedRequest_ThrowsException()
         {
-                        var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
-            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, _storyConverterMock.Object);
+            var _httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+            var _hackerNewsApiService = new HackerNewsAPIService(_loggerMock.Object, _httpClient, new StoryConverter());
 
             // Arrange
             _httpMessageHandlerMock.Protected()
@@ -111,7 +98,7 @@ namespace TopStories.Tests
                 .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.InternalServerError));
 
             // Act & Assert
-            Assert.ThrowsAsync<Exception>(async () => await _hackerNewsApiService.GetTopStoriesIds());
+            Assert.ThrowsAsync<HttpRequestException>(async () => await _hackerNewsApiService.GetTopStoriesIds());
         }
     }
 }
